30325eb269a3b25c3d2e2b8e2117ff0fff927c90
Luis Aguirre <aguirreluis293@gmail.com>
2021-09-23 18:05:54 -0500

    ejercicios js avanzados 23 septiembre

---
+++ ternario-10.js
@@ -0,0 +1,14 @@
+/* Operador ternario
+    es una estructura de flujo(if abreviado)
+     */
+const edad = 20;
+
+/* if(edad >= 18){
+    console.log('Eres mayor de edad.')
+} else{
+    console.log('Eres menor de edad.')
+} */
+
+const mensaje = edad > 18 ? 'Eres mayor de edad' : 'Eres menor de edad'
+
+document.write(`<p>${mensaje}</p>`);
